376fa568315be9504f084c5cc64b75ca30ca9971
prachigulati <prachigulati569@gmail.com>
2025-04-03 11:42:59 +0530

    tweet added

--- app/tweet/forms.py
+++ app/tweet/forms.py
@@ -3,4 +3,4 @@ from .models import Tweet
 class  TweetForm(forms.ModelForm):
     class Meta:
         model=Tweet
-        feilds=['text','photo']
+        fields=['text','photo']
--- app/tweet/urls.py
+++ app/tweet/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
+from .views import *
 
 urlpatterns = [
-    
+    path('', tweet_list, name='tweet_list'),
+    path('create/', tweet_create, name='tweet_create'),
+    path('edit/<int:id>',tweet_edit,name='tweet_edit'),
+    path('delete/<int:id>',tweet_delete, name='tweet_delete'),
 ]
